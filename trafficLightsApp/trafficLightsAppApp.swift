f1ef5092bfa13d04464383f8b4b53a5a64b07759
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-05-12 20:36:35 +0300

    Initial Commit

---
+++ trafficLightsApp/trafficLightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsAppApp.swift
+//  trafficLightsApp
+//
+//  Created by MAC  on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
